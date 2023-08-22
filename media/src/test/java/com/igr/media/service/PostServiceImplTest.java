4a4fd91be320b53c38c6343cc820e61557a55b99
igr76 <gritsuk766@gmail.com>
2023-08-22 21:55:58 +1000

    proseed

--- media/src/test/java/com/igr/media/service/PostServiceImplTest.java
+++ media/src/test/java/com/igr/media/service/PostServiceImplTest.java
@@ -3,8 +3,10 @@ package com.igr.media.service;
 import com.igr.media.dto.PostDto;
 import com.igr.media.dto.UserDto;
 import com.igr.media.entity.Post;
+import com.igr.media.entity.UserEntity;
 import com.igr.media.mapper.PostMapper;
 import com.igr.media.repository.PostRepository;
+import com.igr.media.repository.UserRepository;
 import com.igr.media.service.impl.PostServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +19,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,6 +37,8 @@ public class PostServiceImplTest {
     PostMapper postMapper;
     @Mock
     UserService userService;
+    @Mock
+    UserRepository userRepository;
     int ONE =1;
 @Test
     void getAllPostsTest() {
@@ -54,6 +59,24 @@ public class PostServiceImplTest {
         assertThat(postService.addPost(any(),any(),any())).isEqualTo(getPostDto());
         verify(postRepository,times(ONE)).findAll();
     }
+
+    @Test
+    void updatePostTest() {
+        when(userRepository.findByEmail(any())).thenReturn(Optional.ofNullable(getUser()));
+        when(postRepository.findById(any())).thenReturn(Optional.of(getPost()));
+        assertThat(postService.updatePost(any(),any(),any())).isEqualTo(getPostDto());
+        verify(postRepository,times(ONE)).findAll();
+    }
+
+    private UserEntity getUser() {
+        UserEntity user = new UserEntity();
+        user.setId(1);
+        user.setName("name");
+        user.setEmail("email.ru");
+        user.setPassword("111111");
+        return user;
+    }
+
     private Post getPost() {
         Post savePost = new  Post();
         savePost.setId(1);
