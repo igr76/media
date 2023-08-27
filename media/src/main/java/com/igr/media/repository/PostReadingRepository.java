5bdd57a9b0a37fcca75b315ed58067e2760e4e3b
igr76 <gritsuk766@gmail.com>
2023-08-27 21:02:13 +1000

    proseed

--- media/src/main/java/com/igr/media/repository/PostReadingRepository.java
+++ media/src/main/java/com/igr/media/repository/PostReadingRepository.java
@@ -1,10 +1,11 @@
 package com.igr.media.repository;
 
+import com.igr.media.entity.PostReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PostReadingRepository extends JpaRepository<PostRepository,Integer> {
+public interface PostReadingRepository extends JpaRepository<PostReading,Integer> {
     @Query(nativeQuery = true, value = "DELETE FROM post_reading WHERE user_id = :user")
     void deleteAllByUser_id(@Param("user") int user1);
     @Query(nativeQuery = true, value = "DELETE FROM post_reading WHERE post_id = :post")
--- media/src/main/java/com/igr/media/service/impl/PostServiceImpl.java
+++ media/src/main/java/com/igr/media/service/impl/PostServiceImpl.java
@@ -6,6 +6,7 @@ import com.igr.media.dto.PostDto;
 import com.igr.media.dto.UserDto;
 import com.igr.media.entity.ImageEntity;
 import com.igr.media.entity.Post;
+import com.igr.media.entity.PostReading;
 import com.igr.media.entity.UserEntity;
 import com.igr.media.exception.ElemNotFound;
 import com.igr.media.exception.SecurityAccessException;
@@ -14,6 +15,7 @@ import com.igr.media.mapper.ImageMapper;
 import com.igr.media.mapper.PostMapper;
 import com.igr.media.mapper.UserMapper;
 import com.igr.media.repository.ImageRepository;
+import com.igr.media.repository.PostReadingRepository;
 import com.igr.media.repository.PostRepository;
 import com.igr.media.repository.UserRepository;
 import com.igr.media.service.PostService;
@@ -51,6 +53,8 @@ public class PostServiceImpl implements PostService {
     private ImageRepository imageRepository;
     private UserMapper userMapper;
     private SecurityService securityService;
+    private PostReading postReading;
+    private PostReadingRepository postReadingRepository;
     @Value("${image.ads.dir.path}")
     private String imagePostDir;
 
@@ -70,7 +74,6 @@ public class PostServiceImpl implements PostService {
 
     public Collection<PostDto> getAllPosts(Authentication authentication) {
         log.info(FormLogInfo.getInfo());
-        changeDataTime(authentication);
         Collection<Post> postCollection = postRepository.findAll();
         return postMapper.toDTOList(postCollection);
     }
@@ -273,7 +276,13 @@ public class PostServiceImpl implements PostService {
     public Collection<PostDto> getAllPostsNew(Authentication authentication) {
         log.info(FormLogInfo.getInfo());
         UserEntity user = userRepository.findByName(authentication.getName()).orElseThrow(ElemNotFound::new);
-        Collection<Post> postCollection = postRepository.findAllNew(user.getData());
+        Collection<Post> postCollection = postRepository.getAllPostsNew(user.getId());
+        postCollection.stream()
+                .forEach(e -> {PostReading postReading1 = new PostReading();
+                    postReading1.setUser_id(user.getId());
+                    postReading1.setPost_id(e.getId());
+                    postReading1.setReading(true);
+                    postReadingRepository.save(postReading1);});
         return postMapper.toDTOList(postCollection);
     }
     /**
@@ -282,7 +291,14 @@ public class PostServiceImpl implements PostService {
     public Collection<PostDto> getAllPostsNewSubscriptions(Authentication authentication) {
         log.info(FormLogInfo.getInfo());
         UserEntity user = userRepository.findByName(authentication.getName()).orElseThrow(ElemNotFound::new);
-        return postMapper.toDTOList(postRepository.getAllPostsNewSubscriptions(user.getData()));
+        Collection<Post> postCollection = postRepository.getAllPostsNewSubscriptions(user.getId());
+        postCollection.stream()
+                .forEach(e -> {PostReading postReading1 = new PostReading();
+                postReading1.setUser_id(user.getId());
+                postReading1.setPost_id(e.getId());
+                postReading1.setReading(true);
+                postReadingRepository.save(postReading1);});
+        return postMapper.toDTOList(postCollection);
     }
 
 
