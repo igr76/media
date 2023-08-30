a989d023c2c28facdd4c32789fa50cdcc526be2b
igr76 <gritsuk766@gmail.com>
2023-08-30 21:58:43 +1000

    Реализация  JWT авторизации

---
+++ media/src/main/java/com/igr/media/jwt/JwtUtils.java
@@ -0,0 +1,17 @@
+package com.igr.media.jwt;
+
+import io.jsonwebtoken.Claims;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JwtUtils {
+
+    public static JwtAuthentication generate(Claims claims) {
+
+        final JwtAuthentication jwtInfoToken = new JwtAuthentication();
+        jwtInfoToken.setUserId(claims.getSubject());
+        return jwtInfoToken;
+    }
+
+}
